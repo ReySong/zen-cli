67030b334ce52026e662aea6b062b42e518730c7
reysong <reysong@tencent.com>
2022-04-24 17:40:56 +0800

    feat: modify help text

--- zen.js
+++ zen.js
@@ -6,10 +6,7 @@ const { Command } = require("commander");
 const program = new Command();
 
 const packageJson = require("./package.json");
-
-console.log();
-console.log(chalk.yellow("===== Z E N - C L I ====="));
-console.log();
+const minimist = require("minimist");
 
 //  node版本强制退出
 function checkNodeVersion(wantedVersion, id) {
@@ -20,22 +17,43 @@ function checkNodeVersion(wantedVersion, id) {
             )
         );
         process.exit(1);
-    } else {
-        console.log(chalk.yellow(`You are using Node ${process.version}.\n`));
     }
 }
 
 checkNodeVersion(packageJson.engines.node, "zen-cli");
 
 program
+    .command("create <app-name>")
+    .description("create a new project powered by zen-cli")
+    .option("-f, --force", "Overwrite target directory if it exists")
+    .action((name, options) => {
+        if (minimist(process.argv.slice(3))._.length > 1) {
+            console.log(
+                chalk.yellow(
+                    "\n Info: You provided more than one argument. The first one will be used as the app's name, the rest are ignored."
+                )
+            );
+        }
+        require("./script/create")(name, options);
+    });
+
+program.addHelpText(
+    "before",
+    `\n${chalk.yellow("===== Z E N - C L I =====")}\n\n${chalk.yellow(
+		"You are using Node " + process.version + ".\n"
+	)}`
+);
+
+program
     .version(
-        `${packageJson.version}\n`,
+        `v${packageJson.version}\n`,
         "-v, --version",
         "output the current version"
     )
     .usage("<command> [options]");
 
 program.on("command:*", function() {
+    program.outputHelp();
     console.error(
         chalk.red(
             `Invalid command: ${program.args.join(" ")}\n` +
@@ -51,11 +69,10 @@ program.on("--help", () => {
     console.log(
         `Run ${chalk.cyan(
 			"zen <command> --help"
-		)} for detailed usage of given command`
+		)} for detailed usage of given command.`
     );
     console.log();
 });
+program.commands.forEach(c => c.on("--help", () => console.log())); //  换行
 
-// program.commands.forEach(c => c.on("--help", () => console.log()));
 program.parse(process.argv); // 解析参数
-if (!program.args.length) program.outputHelp();
